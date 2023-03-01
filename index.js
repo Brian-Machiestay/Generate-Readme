a9ade770e02fde1a975230989653bb326414ee7e
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-01 16:00:37 +0000

    write code to append data to readme file

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = ['What is the title of your Readme?',
-"Let's describe your readme. Provide"
-];
+"Let's describe your readme. Provide"];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.appendFile(fileName, data, function(err) {
+        if (err) throw err;
+    })
 }
 
 // function to initialize program
